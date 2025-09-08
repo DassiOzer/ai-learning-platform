8ac51c0e7fa65abf5bf0dfc1fd45d74a8b000f09
Your New Username <your_new_email@example.com>
2025-09-08 17:37:06 +0300

    conection to the databace

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+app.get('/', (req, res) => res.send('API עובד!'));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port http://localhost:${process.env.PORT}`);
+});
